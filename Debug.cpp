846f0d9627224bcd72b6f18074c9b388661b57a8
sephirot47 <victoriodesica777@hotmail.com>
2014-11-24 20:26:28 +0100

    CHEEZY Window! (install SDL libs plz)

--- Debug.cpp
+++ Debug.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 void Debug::Warning(const string &str)
 {
-    cerr << "Warning: " << str << endl;
+    cerr << "Warning(" << __FILE__ << ", " << __LINE__ << "):" << str << endl;
 }
 
 void Debug::Error(const string &str)
 {
-    cerr << "Error: " << str << endl;
+    cerr << "Error(" << __FILE__ << ", " << __LINE__ << "):" << str << endl;
 }
 
 void Debug::Log(Printable &p)
@@ -21,7 +21,6 @@ void Debug::Log(const string str)
 {
     cout << str << endl;
 }
-
 
 
 
--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "SDL2/SDL.h"
 #include "Debug.h"
 #include "Color.h"
 #include "Vector2.h"
@@ -6,13 +7,38 @@
 
 using namespace std;
 
+int InitWindow(SDL_Window **win, SDL_Renderer **ren, int x, int y, int width, int height)
+{
+    if(SDL_Init(SDL_INIT_EVERYTHING) != 0) {Debug::Error("Error: could not open the window!"); return 0;}
+    *win = SDL_CreateWindow("Cheezy window", x, y, width, height, SDL_WINDOW_SHOWN);
+    if(*win == 0) {Debug::Error("Error: could not create the window!"); return 0;}
+    *ren = SDL_CreateRenderer(*win, -1, 0);
+    return 1;
+}
+
 int main()
 {
-    Vector3 v, u, w;
-    v = Vector3(1, 1, 1);
-    u = Vector3(2, 2, 2);
-    w = Vector3(3, 3, 3);
-    Vector2 t(1, 0.5);
-    Color c = Color(4, 5, 6, 1);
-    Debug::Log(3 & v & "asd" & 6 & "sad" & 6 & w & 7.0f);
+    SDL_Window *sdlWin;
+    SDL_Renderer *sdlRen;
+    InitWindow(&sdlWin, &sdlRen, 0, 0, 640, 640);
+    SDL_SetRenderDrawColor(sdlRen, 0, 0, 50, 255);
+
+    bool quit = false;
+    while(not quit)
+    {
+        SDL_Event e;
+        while(SDL_PollEvent(&e))
+        {
+            if(e.type == SDL_QUIT) quit = true;
+        }
+
+        SDL_RenderClear(sdlRen);
+        SDL_RenderPresent(sdlRen);
+        SDL_Delay(25);
+    }
+
+    SDL_DestroyWindow(sdlWin);
+    SDL_Quit();
+
+    return 0;
 }
